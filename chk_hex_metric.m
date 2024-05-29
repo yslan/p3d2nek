edfd4986e31c4e6436b48f9f71a7320becc6454d
yslan <r05246001@ntu.edu.tw>
2024-05-29 00:34:50 -0500

    avoid non-increasing bins for hiscounts

--- chk_hex_metric.m
+++ chk_hex_metric.m
@@ -39,7 +39,7 @@ dat.max_mult=max(accumarray(Hexes(:),1)); % only works for water tight mesh.
 ifhist=0; if(exist('histcounts')==2); ifhist=1; end
 
 if(ifhist==1);
-   stt=@(v,n)histcounts(v,linspace(min(v(:)),max(v(:)),n));
+   stt=@(v,n)histcounts(v,linspace(min(v(:)),max(v(:)+eps),n));
    [c1,e1]=stt(dxmin,N); c1=int8(c1/E*100);
    [c2,e2]=stt(sc_jac,N);c2=int8(c2/E*100);
    [c3,e3]=stt(aratio,N);c3=int8(c3/E*100);
